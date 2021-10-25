f3235cfc3eaf4351ff6ecf3bce284a8ba1da5b77
nejat <nejatoz@gmail.com>
2021-10-24 21:56:09 -0400

    [update] release version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::items_after_statements)]
 // ==============================================================
-#![doc(html_root_url = "https://docs.rs/test-toolbox/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/test-toolbox/0.2.0")]
 
 //! Utility library of helper macros for working with unit tests.
 //!
